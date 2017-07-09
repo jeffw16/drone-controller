c11e1c08cebd392af1b040e5ea76a9379e1fedf0
nefaerious <samshin102@gmail.com>
2017-07-09 12:53:03 -0500

    testing

--- pid-control/pid_calc.js
+++ pid-control/pid_calc.js
@@ -150,9 +150,9 @@ var calculatePID = function(_quaternion, setPoints){
       }
 
     if(i == 3 & bool === true){
-      var thrust = 40;
-      socket.emit("writemotor", {side:"front", thrust:thrust});
-      socket.emit("writemotor", {side:"back", thrust:thrust});
+      var overallPid = pidRoll + pidPitch + pidYaw;
+      socket.emit("writemotor", {side:"front", thrust:overallPid});
+      socket.emit("writemotor", {side:"back", thrust:overallPid});
     }
   }
         // socket.emit("writemotor", {side:"front", thrust:throttle+pidPitch - pidYaw});
